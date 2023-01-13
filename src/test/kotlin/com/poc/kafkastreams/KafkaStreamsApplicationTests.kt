07e8f9dd68fef2642f03b68c6f27f687fb9aeedc
victor <victor.ibanezaparicio@manomano.com>
2023-01-13 09:50:07 +0100

    remove it test

--- src/test/kotlin/com/poc/kafkastreams/KafkaStreamsApplicationTests.kt
+++ src/test/kotlin/com/poc/kafkastreams/KafkaStreamsApplicationTests.kt
@@ -1,12 +1,7 @@
 package com.poc.kafkastreams
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
 class KafkaStreamsApplicationTests {
 
-	@Test
 	fun contextLoads() {
 	}
 
